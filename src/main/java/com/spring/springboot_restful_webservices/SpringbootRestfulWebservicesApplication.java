2fa76d8a65d337defa3e8672605ff50d151f9a62
Ashley <ashnand2000@gmail.com>
2024-09-16 18:00:40 +0530

    Swagger:UI Documentation

--- src/main/java/com/spring/springboot_restful_webservices/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/com/spring/springboot_restful_webservices/SpringbootRestfulWebservicesApplication.java
@@ -1,14 +1,39 @@
 package com.spring.springboot_restful_webservices;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Test Contact",
+						email = "test@gmail.com",
+						url = "website-url"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "website-test-url"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation",
+				url = "website-url"
+		)
+)
 public class SpringbootRestfulWebservicesApplication {
 
-	
+
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
