cb437bb23707ea380b62d9c8eaf0c6352a2d01dc
Ashley <ashnand2000@gmail.com>
2024-09-16 18:12:03 +0530

    Swagger:UI DTO Annotation

--- src/main/java/com/spring/springboot_restful_webservices/dto/UserDto.java
+++ src/main/java/com/spring/springboot_restful_webservices/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.spring.springboot_restful_webservices.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "UserDto Model Information"
+)
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,14 +19,27 @@ import lombok.Setter;
 public class UserDto {
 
 
+    @Schema(
+            description = "User Id"
+    )
     private Long id;
 
+    @Schema(
+
+            description = "User First Name"
+    )
     @NotEmpty(message = "Enter First Name Mandatory")
     private String firstName;
 
+    @Schema(
+            description = "User Last Name"
+    )
     @NotEmpty(message = "Enter Last Name Mandatory")
     private String lastName;
 
+    @Schema(
+            description = "User Email Address"
+    )
     @NotEmpty(message = "Enter Email Mandatory")
     @Email(message = "Enter Valid Email address Mandatory")
     private String email;
